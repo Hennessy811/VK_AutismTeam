b854013a4a48c35e34c7561b32e609fccf3ae9e6
Your Name <you@example.com>
2020-03-27 20:32:56 +0300

    Updates

---
+++ precache-manifest.0ac571077bfa5e49df0bbe36b30c0f5f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c69bcefa99174462f46ebcb14401c6e9",
+    "url": "/VK_AutismTeam/index.html"
+  },
+  {
+    "revision": "345cb65c3ffa0e8e405a",
+    "url": "/VK_AutismTeam/static/css/2.86df01db.chunk.css"
+  },
+  {
+    "revision": "2b1386d0fdd8896d0abb",
+    "url": "/VK_AutismTeam/static/css/main.981f7211.chunk.css"
+  },
+  {
+    "revision": "345cb65c3ffa0e8e405a",
+    "url": "/VK_AutismTeam/static/js/2.93483350.chunk.js"
+  },
+  {
+    "revision": "21ced859ea2b2d6b856d461ad6c2afed",
+    "url": "/VK_AutismTeam/static/js/2.93483350.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2b1386d0fdd8896d0abb",
+    "url": "/VK_AutismTeam/static/js/main.3b151ba8.chunk.js"
+  },
+  {
+    "revision": "9a6646fef70bb8aace4b",
+    "url": "/VK_AutismTeam/static/js/runtime-main.fdb10c53.js"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "/VK_AutismTeam/static/media/persik.4e1ec840.png"
+  }
+]);
